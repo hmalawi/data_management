3845ab603a0033d3fb031e93f53d2c4561bfd3de
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-10-25 12:36:14 -0400

    Facilitates downloading multi-seismograms

---
+++ getseisdata.m
@@ -0,0 +1,25 @@
+function getseisdata(infdir, outfdir, fname, len)
+% getseisdata(infdir, fname, outfdir)
+%
+% This function facilitates downloading multi-seismograms for known 
+% station-event pairs
+%
+%
+% INPUT:
+%
+% infdir     The directory at which the input file is located
+% fname      The name of the input file
+% outfdir    The directory at which the output files will be saved
+% len        The length of the desired seismograms
+%
+% OUTPUT:
+% No arguments will be returned. The seismograms will be saved in (outdir)
+% as SAC files
+%
+% Written by Huda Al Alawi (halawi@princeton.edu) - November 11th, 2020.
+% Last modified by Huda Al Alawi - October 8th, 2021.
+%
+
+
+
+end
