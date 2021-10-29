818ad7120c6e4a47c9528a7eaf7d0ba784dddd9a
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-10-29 11:24:42 -0400

    Facilitates downloading multi-seismograms

--- getseisdata.m
+++ getseisdata.m
@@ -1,5 +1,5 @@
-function getseisdata(infdir, outfdir, fname, len)
-% getseisdata(infdir, fname, outfdir)
+function getseisdata(infdir, outfdir, fname, len, channel)
+% getseisdata(infdir, outfdir, fname, len, channel)
 %
 % This function facilitates downloading multi-seismograms for known 
 % station-event pairs
@@ -10,15 +10,30 @@ function getseisdata(infdir, outfdir, fname, len)
 % infdir     The directory at which the input file is located
 % fname      The name of the input file
 % outfdir    The directory at which the output files will be saved
-% len        The length of the desired seismograms
+% len        The length of the desired seismograms in seconds [defaulted]
+% channel    The channel of interest [defaulted]
 %
 % OUTPUT:
 % No arguments will be returned. The seismograms will be saved in (outdir)
 % as SAC files
 %
-% Written by Huda Al Alawi (halawi@princeton.edu) - November 11th, 2020.
-% Last modified by Huda Al Alawi - October 8th, 2021.
+% SEE ALSO:
+% Requires irisFetch from https://ds.iris.edu/ds/nodes/dmc/manuals/irisfetchm/
 %
+% Written by Huda Al Alawi (halawi@princeton.edu) - October 24, 2021.
+% Last modified by Huda Al Alawi - October 29, 2021.
+%
+
+% Define default values
+defval('channel', 'BH*')
+defval('len', 90)
+
+% Open the file and read the data, skip the headerlines
+% #Network, Station, sLatitude, sLongitude, EventID, tOrigin, eLatitude, eLongitude, Depth(km)
+fid = fopen(strcat(infdir, fname), 'r');
+data = textscan(fid, '%s%s%f%f%d%s%f%f%f', 'HeaderLine', 10);
+
+
 
 
 
