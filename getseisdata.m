841c873b86ce9bad7ba2d125b0eb88e2fc2ef026
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-10-29 13:03:40 -0400

    Facilitates downloading multi-seismograms

--- getseisdata.m
+++ getseisdata.m
@@ -1,4 +1,4 @@
-function getseisdata(infdir, outfdir, fname, len, channel)
+function getseisdata(infdir, outfdir, fname, tstart, tend, channel)
 % getseisdata(infdir, outfdir, fname, len, channel)
 %
 % This function facilitates downloading multi-seismograms for known 
@@ -10,7 +10,12 @@ function getseisdata(infdir, outfdir, fname, len, channel)
 % infdir     The directory at which the input file is located
 % fname      The name of the input file
 % outfdir    The directory at which the output files will be saved
-% len        The length of the desired seismograms in seconds [defaulted]
+% tstart     The relative start time of the requested portion of the
+%            seismogram (in minutes assuming the origin time represents 0
+%            min) [defaulted]
+% tend       The relative end time of the requested portion ot the
+%            seismogram (in minutes assuming the origin time represents 0
+%            min) [defaulted]
 % channel    The channel of interest [defaulted]
 %
 % OUTPUT:
@@ -25,15 +30,16 @@ function getseisdata(infdir, outfdir, fname, len, channel)
 %
 
 % Define default values
+defval('tstart', 15)
+defval('tstart', 25)
 defval('channel', 'BH*')
-defval('len', 90)
 
 % Open the file and read the data, skip the headerlines
 % #Network, Station, sLatitude, sLongitude, EventID, tOrigin, eLatitude, eLongitude, Depth(km)
 fid = fopen(strcat(infdir, fname), 'r');
 data = textscan(fid, '%s%s%f%f%d%s%f%f%f', 'HeaderLine', 10);
 
-
+% 
 
 
 
