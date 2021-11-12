de7728ae0000b9e38d6f1e928339470cd6405e94
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-11-12 09:07:59 -0500

    Facilitates downloading multi-seismograms

--- getseisdata.m
+++ getseisdata.m
@@ -1,5 +1,5 @@
-function getseisdata(infdir, fname, tstart, tend, channel, outfdir, outformat)
-% getseisdata(infdir, outfdir, fname, len, channel)
+function seisdata = getseisdata(infdir, fname, tstart, tend, channel, outfdir, outformat)
+% seisdata = getseisdata(infdir, outfdir, fname, len, channel)
 %
 % This function facilitates downloading multi-seismograms for known 
 % station-event pairs
@@ -7,7 +7,8 @@ function getseisdata(infdir, fname, tstart, tend, channel, outfdir, outformat)
 %
 % INPUT:
 %
-% infdir       The directory at which the input file is located
+% infdir       The directory at which the input file is located (this
+%              should be in the same format of EQDATA output file)
 % fname        The name of the input file
 % tstart       The relative start time of the requested portion of the
 %              seismogram (in minutes assuming the origin time represents 
@@ -17,17 +18,24 @@ function getseisdata(infdir, fname, tstart, tend, channel, outfdir, outformat)
 %              0 min) [defaulted]
 % channel      The channel of interest [defaulted]
 % outfdir      The directory at which the output files will be saved
-% outformat    The 
+% outformat    1 for SAC files
+%              2 for MATLAB structure array
 %
 % OUTPUT:
-% No arguments will be returned. The seismograms will be saved in (outdir)
-% as SAC files
+% seisdata     A structure array that contains time-series data in addition
+%              to station- & earthquake- related information (i.e., name,
+%              location, channel, etc.). This structure will only be
+%              returned if "outformat" is 2.
+%
+% The structure will also be saved to "outfdir". In the case of SAC files,
+% it will be saved to "outfdir".
+% 
 %
 % SEE ALSO:
 % Requires irisFetch from https://ds.iris.edu/ds/nodes/dmc/manuals/irisfetchm/
 %
 % Written by Huda Al Alawi (halawi@princeton.edu) - October 24, 2021.
-% Last modified by Huda Al Alawi - October 29, 2021.
+% Last modified by Huda Al Alawi - November 12, 2021.
 %
 
 % Define default values
