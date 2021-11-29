3f3716bf636029ff6d8381879491e2d9d0cf6b24
Huda Al Alawi <halawi@heinsius.princeton.edu>
2021-11-29 14:17:43 -0500

    Facilitates downloading multi-seismograms

--- getseisdata.m
+++ getseisdata.m
@@ -1,5 +1,7 @@
-function seisdata = getseisdata(infdir, fname, tstart, tend, channel, outfdir, outformat)
-% seisdata = getseisdata(infdir, outfdir, fname, len, channel)
+function seis = getseisdata(infdir, fname, tstart, tend, channel,...
+    outfdir, sizestruct)
+% seis = getseisdata(infdir, fname, tstart, tend, channel,...
+%    outfdir, sizestruct, outformat)
 %
 % This function facilitates downloading multi-seismograms for known 
 % station-event pairs
@@ -18,34 +20,36 @@ function seisdata = getseisdata(infdir, fname, tstart, tend, channel, outfdir, o
 %              0 min): positive after the origin, negetive before the origin [defaulted]
 % channel      The channel of interest [defaulted]
 % outfdir      The directory at which the output files will be saved
-% outformat    1 for MATLAB structure array
-%              2 for SAC files
+% sizestruct   MATLAB arrays have a size limit. If the data is too large,
+%              consider writing the data into multiple structures each of
+%              size "sizestruct" [defaulted: alldata]
 %
 % OUTPUT:
-% seisdata     A structure array that contains tendtime-series data in addition
-%              to station- & earthquake- related information (i.e., name,
-%              location, channel, etc.). This structure will only be
-%              returned if "outformat" is 2.
 %
-% The structure will also be saved to "outfdir". In the case of SAC files,
-% it will be saved to "outfdir".
-% 
+% seis         A nested structure array that contains structures holding
+%              time-series data for each event-station pair in addition to 
+%              station- & earthquake- related information (i.e., name, 
+%              location, channel, etc.). It will also be saved to outfdir. 
+%              There might be empty structures (correspondnig to
+%              no-data-found requests). To obtain SAC files from these 
+%              structures, see irisFetch.Trace2SAC
+%
 %
 % SEE ALSO:
 % Requires irisFetch from https://ds.iris.edu/ds/nodes/dmc/manuals/irisfetchm/
 %
 % Written by Huda Al Alawi (halawi@princeton.edu) - October 24, 2021.
-% Last modified by Huda Al Alawi - November 24 2021.
+% Last modified by Huda Al Alawi - November 29 2021.
 %
 
 % Define default values
 defval('tstart', 15)
-defval('tend', 25)outfdir
+defval('tend', 25)
 defval('channel', 'BHZ')
 
 % Before doing anything, check tstart and tend values
 if tend<tstart
-    disp('tend must be after tstart')
+    disp('end_time must be after start_time')
     return
 end
 
@@ -54,6 +58,9 @@ end
 fid = fopen(strcat(infdir, fname), 'r');
 data = textscan(fid, '%s%s%f%f%d%s%f%f%f', 'HeaderLine', 10);
 fclose(fid);
+
+% One last default value
+defval('sizestruct', length(data{1}))
 
 % Assign the data that we need to meaningful variables for easy access when
 % call irisFetch
@@ -83,67 +90,58 @@ end
 t1 = string(datestr(t1, 'yyyy-mm-dd HH:MM:SS'));
 t2 = string(datestr(t2, 'yyyy-mm-dd HH:MM:SS'));
 
-% Depending on the chosen "outformat", start requesting the data
-switch outformat
-    case 1
-        % Let the structure hold 20k everytime (will make it usder-defined
-        % limit later)
-        sizestruct = 20000;
-        % See how many 20ks in the array we have, and decide the number of
-        % loop iterations accordingly
-        if mod(length(net), sizestruct) ~= 0
-            num = round(length(net)/sizestruct) + 1;
-        else
-            num = length(net)/sizestruct;
-        end
-        % Initialize a structure
-        seis(sizestruct) = struct();
-        
-        % Now need to loop to save the structure every 20k
-        % Set a variable for counting 20ks
-        count20k = 0;
-        for jj = 1:num
-            % Define the new arrays
-            % The starting and ending index...
-            ind1 = (count20k*20000)+1;
-            ind2 = ind1+sizestruct-1;
-            % Check if the ending index exceeded the length of the original
-            % data. Define the new arrays accordingly
-            if ind2 <= length(net)
-                newnet = net(ind1:ind2);
-                newsta = sta(ind1:ind2);
-                newt1 = t1(ind1:ind2);
-                newt2 = t2(ind1:ind2);
-            else
-                newnet = net(ind1:end);
-                newsta = sta(ind1:end);
-                newt1 = t1(ind1:end);
-                newt2 = t2(ind1:end);
-            end 
 
-            % For all event-station pairs in the file, call irisFetch to get 
-            % the data and store them into a structure array
-            for ii = 1:sizestruct
-                ii
-                try
-                    tr = irisFetch.Traces(newnet(ii), newsta(ii), '*', channel, newt1(ii), newt2(ii));
-                catch
-                    % If couldn't find the data, assign the index to 1 and skip
-                    continue
-                end
-                % If data was found, store it into a structure
-                seis(ii).evesta = tr;
-            end
-            % Save the structure
-            save(sprintf('%sdata%d', outfdir, count20k), 'seis');
-            % Clear the structure
-            clear seis
-            % Update the count
-            count20k = count20k + 1;
+% See how many "sizestruct" in the array we have, and decide the number of
+% loop iterations accordingly
+if mod(length(net), sizestruct) ~= 0
+    num = round(length(net)/sizestruct) + 1;
+else
+    num = length(net)/sizestruct;
+end
+% Initialize a structure, maybe need to move this later
+seis(sizestruct) = struct();
+
+% Now need to loop to save the structure every "sizestruct"
+% Set a variable for counting
+countnum = 0;
+for jj = 1:num
+    % Define the new arrays
+    % The starting and ending index...
+    ind1 = (countnum*sizestruct)+1;
+    ind2 = ind1+sizestruct-1;
+    % Check if the ending index exceeded the length of the original
+    % data. Define the new arrays accordingly
+    if ind2 <= length(net)
+        newnet = net(ind1:ind2);
+        newsta = sta(ind1:ind2);
+        newt1 = t1(ind1:ind2);
+        newt2 = t2(ind1:ind2);
+    else
+        newnet = net(ind1:end);
+        newsta = sta(ind1:end);
+        newt1 = t1(ind1:end);
+        newt2 = t2(ind1:end);
+    end 
+
+    % For all event-station pairs in the file, call irisFetch to get 
+    % the data and store them into a structure array
+    for ii = 1:sizestruct
+        ii
+        try
+            tr = irisFetch.Traces(newnet(ii), newsta(ii), '*', channel, newt1(ii), newt2(ii));
+        catch
+            % If couldn't find the data, assign the index to 1 and skip
+            continue
         end
-        
-    case 2
-        
+        % If data was found, store it into a structure
+        seis(ii).evesta = tr;
+    end
+    % Save the structure
+    save(sprintf('%sdata%d', outfdir, countnum), 'seis');
+    % Clear the structure
+    clear seis
+    % Update the count
+    countnum = countnum + 1;
 end
 
 
